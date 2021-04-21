51fbdf8056553d113622456558e7826db02c1150
Thanuja94 <thanujag94@gmail.com>
2021-04-22 02:29:15 +0530

    adding controllers and classes
    navigation controller, home,admin signup view contrlller classes

---
+++ WDP/Controller/AdminViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AdminViewController.swift
+//  WDP
+//
+//  Created by Thanuja Nayanajith on 4/22/21.
+//  Copyright © 2021 NIBM. All rights reserved.
+//
+
+import UIKit
+
+class AdminViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
