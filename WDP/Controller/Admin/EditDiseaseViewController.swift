19b90f2ac45ae6aea4f42478693936a72eb28735
Thanuja94 <thanujag94@gmail.com>
2021-04-28 14:48:40 +0530

    Add EditDisease view controller

---
+++ WDP/Controller/Admin/EditDiseaseViewController.swift
@@ -0,0 +1,53 @@
+//
+//  EditDiseaseViewController.swift
+//  WDP
+//
+//  Created by Thanuja Nayanajith on 4/28/21.
+//  Copyright © 2021 NIBM. All rights reserved.
+//
+
+import UIKit
+
+class EditDiseaseViewController: UIViewController {
+    
+    // MARK: - Properties
+
+    @IBOutlet weak var DiseaseNameLabel: UILabel!
+    
+    
+    @IBOutlet weak var DiseaseNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var SymptomLabel: UILabel!
+    
+    @IBOutlet weak var SymptomTextView: UITextView!
+    
+    
+    @IBOutlet weak var PrecautionLabel: UILabel!
+    
+    
+    @IBOutlet weak var PrecautionTextView: UITextView!
+    
+    @IBOutlet weak var DeleteDiseaseButton: UIButton!
+    
+    
+    @IBOutlet weak var SaveChangesButton: UIButton!
+    
+    
+    
+    // MARK: - Lifecycale
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setupUI()
+    }
+    
+    // MARK: - Functions
+    
+    func setupUI()   {
+        Utilities.filledButton(button: DeleteDiseaseButton)
+        Utilities.filledButton(button: SaveChangesButton)
+         
+    }
+}
