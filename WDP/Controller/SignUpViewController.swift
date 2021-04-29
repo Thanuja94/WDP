68d283f69aeb73ae48d2705d5948879e8270a062
Thanuja94 <thanujag94@gmail.com>
2021-04-29 19:41:35 +0530

    modify sign up view controller,

--- WDP/Controller/SignUpViewController.swift
+++ WDP/Controller/SignUpViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class SignUpViewController: UIViewController {
     
@@ -48,6 +50,46 @@ class SignUpViewController: UIViewController {
     
     
     @IBAction func HandleSignUP(_ sender: Any) {
+        
+        guard let firstname = FirstNameTextField.text else { return }
+        guard let lastname = LastNameTextField.text else { return }
+        guard let email = EmailTextField.text else { return }
+        guard let password = PasswordTextField.text else { return }
+        guard let occupation = OccupationTextField.text else { return }
+        
+
+        
+        let values = [
+        "firstname": firstname,
+        "lastname": lastname,
+        "email": email,
+        "occuption": occupation
+        
+        
+        ] as [String : Any]
+        
+         print("DEBUG: Sign up click")
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+            if let error = error {
+                           print("DEBUG: Faild to register user with error \(error)")
+                           return
+                       }
+             guard let uid = result?.user.uid else { return }
+            
+            Database.database().reference().child("users").child(uid).updateChildValues(values){ (error, ref) in
+                
+                print("DEBUG: Successfully registered and save data ")
+                
+                if let error = error {
+                                         print("DEBUG: failto save \(error)")
+                                         return
+                                     }
+            }
+            
+        }
+        
+//        print("DEBUG: user ", values)
     }
     
     
