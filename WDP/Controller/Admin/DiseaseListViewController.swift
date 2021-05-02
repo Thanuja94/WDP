5dc3ec367e20a27cf6a127bcc3ea3cbb306804b3
Thanuja94 <thanujag94@gmail.com>
2021-05-02 13:21:56 +0530

    create user list view controller, load users from db

---
+++ WDP/Controller/Admin/DiseaseListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DiseaseListViewController.swift
+//  WDP
+//
+//  Created by Thanuja Nayanajith on 5/2/21.
+//  Copyright © 2021 NIBM. All rights reserved.
+//
+
+import UIKit
+
+class DiseaseListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
